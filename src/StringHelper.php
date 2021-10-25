533ee1afe66b73a5df79a137044d23e077b7eae3
Leonid Sheikman <leonid@sheikman.ru>
2021-10-25 19:04:47 +0300

    add truncateString

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -1,7 +1,6 @@
 <?php declare( strict_types=1 );
 /**
  * Helper class for processing strings
- * Класс для обработки строк
  *
  * This file is part of the project.
  *
@@ -18,62 +17,75 @@ namespace Leonid74\Helpers;
 class StringHelper
 {
     /**
-     * Add trailing slash to the string
-     * Добавление слэша к концу строки.
+     * Add a trailing slash to the string
      *
-     * @param string $str
+     * @param string $sString
      *
      * @return string
-     *
-     * @author Leonid Sheikman <leonid@sheikman.ru>
      */
-    public static function addTrailingSlash( string $str = '' ): string
+    public static function addTrailingSlash( string $sString = '' ): string
     {
-        return \rtrim( $str, '/' ) . '/';
+        return \rtrim( $sString, '/' ) . '/';
     }
 
     /**
-     * Remove trailing slash from the string
-     * Удаление слэша в конце строки.
+     * Remove a trailing slash from the string
      *
-     * @param string $str
+     * @param string $sString
      *
      * @return string
-     *
-     * @author Leonid Sheikman <leonid@sheikman.ru>
      */
-    public static function removeTrailingSlash( string $str = '' ): string
+    public static function removeTrailingSlash( string $sString = '' ): string
     {
-        return \rtrim( $str, '/' );
+        return \rtrim( $sString, '/' );
     }
 
     /**
-     * Add slash to the beginning of the string
-     * Добавление слэша к началу строки.
+     * Add a slash to the beginning of the string
      *
-     * @param string $str
+     * @param string $sString
      *
      * @return string
-     *
-     * @author Leonid Sheikman <leonid@sheikman.ru>
      */
-    public static function prependSlash( string $str = '' ): string
+    public static function prependSlash( string $sString = '' ): string
     {
-        return '/' . \ltrim( $str, '/' );
+        return '/' . \ltrim( $sString, '/' );
     }
 
     /**
      * Remove BOM from the string
-     * Удаление BOM из строки.
      *
-     * @param string $str
+     * @param string $sString
      *
      * @return string
-     *
-     * @author Leonid Sheikman <leonid@sheikman.ru>
      */
-    public static function removeBOM( string $str = '' ): string
+    public static function removeBOM( string $sString = '' ): string
     {
-        return 0 === \strncasecmp( \pack( 'CCC', 0xef, 0xbb, 0xbf ), $str, 3 ) ? \mb_substr( $str, 3 ) : $str;
+        return 0 === \strncasecmp( \pack( 'CCC', 0xef, 0xbb, 0xbf ), $sString, 3 ) ? \mb_substr( $sString, 3 ) : $sString;
+    }
+
+    /**
+     * Truncates the string to the specified length with the specified suffix (optional).
+     *
+     * StringHelper::truncateString('Lorem ipsum inum', 10);         // returns 'Lorem i...'
+     * StringHelper::truncateString('Lorem ipsum inum', 15, '>>>');  // returns 'Lorem ipsum >>>'
+     *
+     * @param string $sString
+     * @param int    $iLength
+     * @param string $sSuffix (optional, defaults to '...')
+     *
+     * @return string
+     */
+    public static function truncateString( string $sString, int $iLength, ?string $sSuffix = '...' ): string
+    {
+        $iLengthOfSuffix = \mb_strlen( $sSuffix );
+
+        if ( \mb_strlen( $sString ) <= ( $iLength - $iLengthOfSuffix ) ) {
+            return $sString;
+        }
+
+        $sTruncatedString = \mb_substr( $sString, 0, $iLength - $iLengthOfSuffix );
+
+        return $sTruncatedString . $sSuffix;
     }
 }
