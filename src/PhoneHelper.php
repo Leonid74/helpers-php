b5d1cc29c40ad547965e6927ab57ae14f5fc88b7
Leonid Sheikman <leonid@sheikman.ru>
2024-05-23 15:54:48 +0300

    add new methods to PhoneHelper

--- src/PhoneHelper.php
+++ src/PhoneHelper.php
@@ -19,6 +19,130 @@ namespace Leonid74\Helpers;
 
 class PhoneHelper
 {
+    private static $countryCodes = [
+        '1'   => 'США/Канада',
+        '7'   => 'Россия/Казахстан',
+        '20'  => 'Египет',
+        '21'  => 'Южная Африка',
+        '22'  => 'Марокко',
+        '23'  => 'Алжир',
+        '24'  => 'Замбия',
+        '25'  => 'Зимбабве',
+        '26'  => 'Мозамбик',
+        '27'  => 'Южная Африка',
+        '28'  => 'Эритрея',
+        '29'  => 'Эфиопия',
+        '30'  => 'Греция',
+        '31'  => 'Нидерланды',
+        '32'  => 'Бельгия',
+        '33'  => 'Франция',
+        '34'  => 'Испания',
+        '36'  => 'Венгрия',
+        '39'  => 'Италия',
+        '40'  => 'Румыния',
+        '41'  => 'Швейцария',
+        '43'  => 'Австрия',
+        '44'  => 'Великобритания',
+        '45'  => 'Дания',
+        '46'  => 'Швеция',
+        '47'  => 'Норвегия',
+        '48'  => 'Польша',
+        '49'  => 'Германия',
+        '50'  => 'Бангладеш',
+        '51'  => 'Перу',
+        '52'  => 'Мексика',
+        '53'  => 'Куба',
+        '54'  => 'Аргентина',
+        '55'  => 'Бразилия',
+        '56'  => 'Чили',
+        '60'  => 'Малайзия',
+        '61'  => 'Австралия',
+        '62'  => 'Индонезия',
+        '63'  => 'Филиппины',
+        '64'  => 'Новая Зеландия',
+        '65'  => 'Сингапур',
+        '66'  => 'Таиланд',
+        '81'  => 'Япония',
+        '82'  => 'Южная Корея',
+        '84'  => 'Вьетнам',
+        '86'  => 'Китай',
+        '90'  => 'Турция',
+        '91'  => 'Индия',
+        '92'  => 'Пакистан',
+        '93'  => 'Афганистан',
+        '94'  => 'Шри-Ланка',
+        '95'  => 'Мьянма',
+        '98'  => 'Иран',
+        '370' => 'Литва',
+        '371' => 'Латвия',
+        '372' => 'Эстония',
+        '373' => 'Молдова',
+        '374' => 'Армения',
+        '375' => 'Беларусь',
+        '376' => 'Андорра',
+        '377' => 'Монако',
+        '378' => 'Сан-Марино',
+        '380' => 'Украина',
+        '381' => 'Сербия',
+        '382' => 'Черногория',
+        '383' => 'Косово',
+        '385' => 'Хорватия',
+        '386' => 'Словения',
+        '387' => 'Босния и Герцеговина',
+        '389' => 'Северная Македония',
+        '420' => 'Чехия',
+        '421' => 'Словакия',
+        '423' => 'Лихтенштейн',
+        '994' => 'Азербайджан',
+        '995' => 'Грузия',
+        '996' => 'Киргизия',
+        '998' => 'Узбекистан',
+    ];
+
+    /**
+     * Formats an international mobile phone number by removing non-numeric characters,
+     * ensuring it starts with a plus sign, and determining the country based on the phone number.
+     *
+     * Форматируем международный мобильный номер телефона, удаляя нечисловые символы,
+     * добавляя знак плюс в начале и определяя страну по номеру телефона.
+     *
+     * @param string|null $phoneNumber the original phone number
+     *
+     * @return array|null an array with the formatted phone number and country code, or null if the number is invalid
+     */
+    public static function formatInternationalMobilePhoneNumber(?string $phoneNumber): ?array
+    {
+        if (empty($phoneNumber)) {
+            return null;
+        }
+
+        // Remove everything except numbers and plus sign
+        $phoneNumber = \preg_replace('/[^\d+]/', '', $phoneNumber);
+
+        // If the number begins with 8 and has a length of 11 characters, replace 8 by +7 (for Russian numbers)
+        if ($phoneNumber[0] === '8' && \strlen($phoneNumber) === 11) {
+            $phoneNumber = '+7' . \substr($phoneNumber, 1);
+        }
+
+        // Add plus sign if not present
+        if ($phoneNumber[0] !== '+') {
+            $phoneNumber = '+' . $phoneNumber;
+        }
+
+        // Determine the country code based on the phone number
+        $countryCode = self::getCountryCodeFromPhoneNumber($phoneNumber);
+
+        if ($countryCode === null) {
+            return null;
+        }
+
+        return [
+            'formattedNumber' => $phoneNumber,
+            'countryCode'     => $countryCode,
+            'countryName'     => self::$countryCodes[$countryCode],
+        ];
+    }
+
     /**
      * Formats a Russian mobile phone number by removing non-numeric characters and converting it starting with +7.
      *
@@ -155,5 +279,26 @@ class PhoneHelper
         }
 
         return $oResult;
+    }
+
+    /**
+     * Determines the country code from the phone number.
+     *
+     * Определяем код страны по номеру телефона.
+     *
+     * @param string $phoneNumber the formatted phone number
+     *
+     * @return string|null the country code, or null if the country code cannot be determined
+     */
+    private static function getCountryCodeFromPhoneNumber(string $phoneNumber): ?string
+    {
+        // Extract the country code from the phone number
+        foreach (self::$countryCodes as $code => $country) {
+            if (\str_starts_with($phoneNumber, '+' . $code)) {
+                return $code;
+            }
+        }
+
+        return null;
     }
 }
